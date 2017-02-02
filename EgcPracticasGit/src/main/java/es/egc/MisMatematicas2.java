216f2075ebd646d94d83b80a5074fda46b477e9c
dylanforit <dylanmorenotellez@gmail.com>
2017-02-02 03:17:38 +0100

    Metodos Para la clase Añadida MisMatematicas2

--- EgcPracticasGit/src/main/java/es/egc/MisMatematicas2.java
+++ EgcPracticasGit/src/main/java/es/egc/MisMatematicas2.java
@@ -1,5 +1,36 @@
 package es.egc;
+import org.apache.log4j.*; 
 
-public class MisMatematicas2 {
-
+/**
+ * DIVISION
+ *
+ */
+public class MisMatematicas2 
+{
+	static Logger log=Logger.getLogger(MisMatematicas.class); 
+	
+	public static int divide2(int a, int b){
+		int result= a/b;
+		log.info("Returning "+a+"/"+b+"="+result);
+		return result;
+		
+	}
+	
+	public static int multiplica2(int a, int b){
+		int result= a*b;
+		log.info("Returning "+a+"*"+b+"="+result);
+		return result;
+	}
+	
+	public static int suma2(int a, int b){
+		int result = a+b
+		log.info("Returning" +a+ "+" +b+"=" result);
+		return result;
+	}
+	
+	public static int resta2(int a, int b){
+		int result = a-b
+		log.info("Returning" +a+ "-" +b+"=" result);
+		return result;
+	}
 }
