26d148a9fb550b4effaeca69758f2179f72b557e
dylanforit <dylanmorenotellez@gmail.com>
2017-02-02 03:14:16 +0100

    Método suma añadido

--- EgcPracticasGit/src/main/java/es/egc/MisMatematicas.java
+++ EgcPracticasGit/src/main/java/es/egc/MisMatematicas.java
@@ -1,10 +1,15 @@
 package es.egc;
 
-
+import org.apache.log4j.*; 
 /**
  * MULTIPLICACIÓN
  *
  */
 public class MisMatematicas {
 
+	public static int suma(int a, int b){
+		int result = a+b
+		log.info("Returning" +a+ "+" +b+"=" result);
+		return result;
+	}
 }
