2ec16a893d597a2faebcc4a4cc881f70f60c41f5
dylanforit <dylanmorenotellez@gmail.com>
2017-02-02 03:44:03 +0100

    Cambio de nombre a una rama

--- EgcPracticasGit/src/test/java/es/egc/Test1.java
+++ EgcPracticasGit/src/test/java/es/egc/Test1.java
@@ -15,7 +15,7 @@ public class Test1
      *
      * @param testName name of the test case
      */
-    public Test1( String testName )
+    public Test1Conflictivo( String testName )
     {
         super( testName );
     }
